39769f8074ac78322ff20b4774a54dc0a61cd297
Manfred Lohöfener <manfred@lohoefener-leipzig.de>
2018-10-30 08:52:31 +0100

    Add files via upload

--- Test_MQTT_pub.m
+++ Test_MQTT_pub.m
@@ -1,20 +1,28 @@
-% Test_MQTT_pub.m Script for testing MQTT
-% Voraussetzungen:
-% 1. Linux installieren
-% 2. Paket mosquitto-clients installieren
-% 3. GNU Octave oder MATLAB installieren.
+% Test_MQTT_pub.m - Script for testing MQTT
+% Prerequisites:
+% 1. Boot Linux
+% 2. Install mosquitto-clients
+% 3. Install GNU Octave or MATLAB
 % 01.12.2017, Manfred Lohöfener, HoMe
+% Default TCP-Port: 1883
 % http://www.mqtt-dashboard.com/
-% TCP-Port: 1883
 
 clear
-%host = 'test.mosquitto.org';
-%host = 'broker.hivemq.com';
-host = 'iot.hs-merseburg.de';
-topic = 'Uhrzeit';
 
-for c = 1:20                                  % Counter
+%host = '192.168.1.122';
+%host = 'localhost';
+%host = '10.42.0.1';                        % my_free_wifi
+host = 'test.mosquitto.org';
+%host = 'broker.hivemq.com';
+%host = 'iot.hs-merseburg.de';
+
+%topic = 'Uhrzeit';
+topic = 'HoMe18';
+%topic = 'Advantech/00D0C9FAD5D3/data';     % WISE-4012 Development Kit
+%topic = 'Advantech/#';
+
+for c = 1:20                                % Counter
   testMessage = datestr (now, 'dd.mm.yyyy HH:MM:SS');
   mqtt_pub (host, topic, testMessage);
-  pause (2)                                   % 2 s Pause
+  pause (2)                                 % 2 s break
 end
--- Test_MQTT_sub.m
+++ Test_MQTT_sub.m
@@ -1,26 +1,36 @@
-% Test_MQTT_sub.m Script for testing MQTT
-% Voraussetzungen:
-% 1. Linux installieren
-% 2. Paket mosquitto-clients installieren
-% 3. GNU Octave oder MATLAB installieren.
+% Test_MQTT_sub.m - Script for testing MQTT
+% Prerequisites:
+% 1. Boot Linux
+% 2. Install mosquitto-clients
+% 3. Install GNU Octave or MATLAB
 % 01.12.2017, Manfred Lohöfener, HoMe
-% TCP-Port: 1883
+% Default TCP-Port: 1883
+% http://www.mqtt-dashboard.com/
 
 clear
-%host = 'test.mosquitto.org';
+
+%host = '192.168.1.122';
+%host = 'localhost';
+%host = '10.42.0.1';                        % my_free_wifi
+host = 'test.mosquitto.org';
 %host = 'broker.hivemq.com';
-host = 'iot.hs-merseburg.de';
-topic = 'Uhrzeit';
+%host = 'iot.hs-merseburg.de';
+
+%topic = 'Uhrzeit';
+topic = 'HoMe18';
+%topic = 'Advantech/00D0C9FAD5D3/data';     % WISE-4012 Development Kit
+%topic = 'Advantech/#';
+
 puf = {0};                                  % Message-Puffer
 
 for c = 1:20                                % Counter
-    [stat, txt] = mqtt_sub (host, topic);   % Uhrzeit empfangen
+    [stat, txt] = mqtt_sub (host, topic);   % receive message
     testMessage = datestr (now, 'dd.mm.yyyy HH:MM:SS');
-    puf (c) = {[testMessage ' : ' txt]};    % Empfangs- und Sendezeit
+    puf (c) = {[testMessage ' : ' txt]};    % timestamps of receive and send
 end
 
-disp (puf');                                % auf Konsole
+disp (puf');                                % on command window
 fid = fopen ('puf.txt', 'w');
-fprintf (fid, '%s\n', '    Empfangszeit    :     Sendezeit');
-fprintf (fid, '%s', puf{:});                % und in Datei
+fprintf (fid, '%s\n', '      Received      :       Sent   ');
+fprintf (fid, '%s', puf{:});                % and in file
 fclose (fid);
